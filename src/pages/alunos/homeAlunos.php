0b86598346a38bb9c3f892b8b5b7098584e295fe
TwistGE <otaviotm09@gmail.com>
2025-06-16 20:10:18 -0300

    php includes

---
+++ src/pages/alunos/homeAlunos.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sanquim | Home</title>
+  <link rel="stylesheet" href="../../styles/Malunos.css">
+  <link rel="stylesheet" href="../../styles/homeAlunos.css">
+  <script src="https://kit.fontawesome.com/b2800b7110.js" crossorigin="anonymous" defer></script>
+</head>
+<body>
+  <?php include '../../Headers/Malunos.html'; ?>
+</body>
+</html>
