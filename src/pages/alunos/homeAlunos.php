732b6b6003ef28db23c17fa3e6533d5f19593957
TwistGE <otaviotm09@gmail.com>
2025-06-16 21:28:49 -0300

    help css

--- src/pages/alunos/homeAlunos.php
+++ src/pages/alunos/homeAlunos.php
@@ -3,12 +3,68 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Sanquim | Home</title>
+  <title>Aluno | Home</title>
   <link rel="stylesheet" href="../../styles/Malunos.css">
-  <link rel="stylesheet" href="../../styles/homeAlunos.css">
+  <link rel="stylesheet" href="../../styles/homeAluno.css">
   <script src="https://kit.fontawesome.com/b2800b7110.js" crossorigin="anonymous" defer></script>
 </head>
 <body>
   <?php include '../../Headers/Malunos.html'; ?>
+  <main>
+    <div class="content">
+
+      <div class="colum1">
+        <div class="container_aluno island">
+          <!-- Lado Esquerdo -->
+          <div class="aluno_info_pessoal">
+            <div class="foto_aluno">
+              <img src="../../images/profile.webp" alt="Foto do Aluno">
+            </div>
+            <div class="dados_aluno">
+              <h3>João Silva</h3>
+              <p><strong>RM:</strong> 123456</p>
+              <p><strong>CPF:</strong> 123.456.789-00</p>
+              <p><strong>Ciclos:</strong> Ensino Médio / Técnico</p>
+            </div>
+          </div>
+          
+          <!-- Lado Direito -->
+          <div class="aluno_info_curso">
+            <h3>Informações do Curso</h3>
+            <p><strong>Curso:</strong> Técnico em Informática Integrado ao Ensino Médio</p>
+            <p><strong>Vestibular:</strong> Processo Seletivo 2023</p>
+            <p><strong>Pré-Vestibulinho:</strong> Participou no 2º semestre de 2022</p>
+            <p><strong>Período:</strong> Manhã</p>
+            <p><strong>Status:</strong> Matriculado</p>
+          </div>
+        </div>
+      </div>
+      <div class="colum2 island">
+        <div class="planos-top">
+          <h3>Próximas Aulas</h3>
+        </div>
+        <div class="planos_conteudo">
+          <ul>
+            <li>
+              <div class="profile">
+
+                <div class="Menu_profile-icon">
+                  <img src="../../images/profile.webp" alt="Foto de perfil">
+                </div>
+                <div class="solicitacoes-texts">
+                  <h3>Julia da Silva </h3>
+                  <span>
+                    Solicitação de documento
+                  </span>
+                  
+                </div>
+              </div>
+            </li>
+          </ul>
+      </div>
+    </div>
+  </div>
+  </main>
 </body>
 </html>
+  
